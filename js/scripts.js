cf3ee5ff4b924025c74ac5bccfd52262f7bb82c5
Wess58 <wesleymbate@gmail.com>
2019-02-16 14:14:43 +0300

    final push

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// $(document).ready(function (){
+//   $(".btn").onclick(function(event){
+//     alert("you have entered Email")
+//   });
+//         event.preventDefault();
+// });
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
